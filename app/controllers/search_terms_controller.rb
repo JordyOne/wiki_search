efddfd602098dd9c520d109f8ac1133f9d05b0ab
JordyOne <jordan.t.dale@gmail.com>
2017-07-19 12:31:51 -0600

    pulled form into partial

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -14,6 +14,7 @@ class SearchTermsController < ApplicationController
 
   def show
     @search_term = SearchTerm.find(params[:id])
+    @new_search_term = SearchTerm.new
   end
 
   private
--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -9,4 +9,16 @@ feature "Search", :type => :feature do
 
     expect(page).to have_text("Test search")
   end
+
+  scenario "User is viewing existing search" do
+    search_term = SearchTerm.new(term: "Test search")
+    visit search_term_path(search_term)
+
+    expect(page).to have_selector("h1", text: search_term.term)
+
+    fill_in "Term", :with => "Another Test search"
+    click_button "Search"
+
+    expect(page).to have_text("Another Test search")
+  end
 end
